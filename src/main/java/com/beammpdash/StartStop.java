d4d7f59214b311411854687e7e62fd8819f8480e
TactlessMutation <github21xyz@proton.me>
2026-02-24 09:13:09 -0800

    Added comment explaining the application of the start/stop class.

--- src/main/java/com/beammpdash/StartStop.java
+++ src/main/java/com/beammpdash/StartStop.java
@@ -6,18 +6,19 @@ import java.io.IOException;
 
 public class StartStop 
 {
-
+    // Host script can watch for modifictions to file and run commands to start/stop/restart
+    // the beamMP container.
     private static final String FILE_PATH = "start-stop-restart.txt";
 
     private static synchronized String writeCommand(String command) 
     {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) 
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) 
         {
             writer.write(command);
             writer.flush();
             return "Success!";
         } 
-        catch (IOException e) 
+        catch(IOException e) 
         {
             e.printStackTrace();
             return "Failed: " + e.getMessage();
